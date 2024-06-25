ddcdee2ce769d34d05af05fb0a823c83d733ce7b
TetianaKhomiak <tkhomiak8889@gmail.com>
2024-06-25 18:13:08 +0200

    add .

--- src/components/TaskSearch.jsx
+++ src/components/TaskSearch.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "../styles/TaskSearch.css";
+import { IoIosSearch } from "react-icons/io";
 
 const TaskSearch = () => {
   return (
     <div className="search__wrapper">
-      <input type="text" placeholder="search" />
+      <input className="search__input" type="text" placeholder="search" />
+      <IoIosSearch className="search__icon" />
     </div>
   );
 };
