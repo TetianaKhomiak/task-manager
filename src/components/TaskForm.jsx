e83d63889e2a44786ddc84e361097173587421d4
TetianaKhomiak <tkhomiak8889@gmail.com>
2024-06-27 18:44:42 +0200

    add .

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -8,12 +8,16 @@ const TaskForm = ({ setIsAddingTask, columnName }) => {
   const [taskName, setTaskName] = useState("");
   const [taskDescription, setTaskDescription] = useState("");
   const [charCount, setCharCount] = useState(20);
+  const [isError, setIsError] = useState(false);
   const id = useId();
   const dispatch = useDispatch();
 
   const handleSubmitForm = (e) => {
     e.preventDefault();
-    if (taskName !== "") {
+    if (taskName === "") {
+      setIsError(true);
+    } else {
+      setIsError(false);
       dispatch(
         addTask({
           name: taskName,
@@ -25,12 +29,13 @@ const TaskForm = ({ setIsAddingTask, columnName }) => {
           selectedColor: "",
         })
       );
-    }
 
-    setTaskName("");
-    setTaskDescription("");
-    setCharCount(20);
-    setIsAddingTask(false);
+      // Clear form fields and state after adding task
+      setTaskName("");
+      setTaskDescription("");
+      setCharCount(20);
+      setIsAddingTask(false);
+    }
   };
 
   const handleCancel = () => {
@@ -40,6 +45,7 @@ const TaskForm = ({ setIsAddingTask, columnName }) => {
   const handleNameChange = (e) => {
     setTaskName(e.target.value);
     setCharCount(20 - e.target.value.length);
+    setIsError(e.target.value === "");
   };
 
   const handleDescriptionChange = (e) => {
@@ -49,17 +55,17 @@ const TaskForm = ({ setIsAddingTask, columnName }) => {
   return (
     <div className="form__wrapper">
       <form className="form" onSubmit={handleSubmitForm}>
-        <p className="form__text">Title:</p>
         <textarea
           maxLength={20}
           className="form__textarea"
           value={taskName}
+          placeholder={isError ? "Title is required" : "Title"}
           onChange={handleNameChange}></textarea>
         <div className="char-count">Characters left: {charCount}</div>
-        <p className="form__text">Description:</p>
         <textarea
           className="form__textarea form__textarea_big"
           value={taskDescription}
+          placeholder="Description"
           onChange={handleDescriptionChange}></textarea>
         <div className="form__btn_wrapper">
           <button className="form__btn" type="submit">
